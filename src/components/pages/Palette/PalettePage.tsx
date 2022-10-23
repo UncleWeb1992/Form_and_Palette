8eae6f3ed864fd392c3b578ee4d8ef6b00995fc7
Иванов Тахир <watefalls@mail.ru>
2022-10-23 21:17:43 +0500

    fix

--- src/components/pages/Palette/PalettePage.tsx
+++ src/components/pages/Palette/PalettePage.tsx
@@ -13,19 +13,23 @@ const PalettePage: FC = () => {
   const [hidden, setHidden] = useState(true);
 
   const addedColor = () => {
-    setHidden(false);
     const data = {
       id: uuid(),
       color,
     };
 
-    if (!hidden) {
+    if (hidden && !paletteList.length) {
+      setHidden(false);
+      dispatch(addColor(data));
+    } else if (hidden && paletteList.length) {
+      setHidden(false);
+    } else {
       dispatch(addColor(data));
     }
   };
 
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between" onClick={() => setHidden(true)}>
       <div
         className="w-[35%] mt-4 py-2 px-4"
         onClick={(e) => e.stopPropagation()}
@@ -34,15 +38,10 @@ const PalettePage: FC = () => {
         <Button onClick={addedColor}>Добавить цвет</Button>
       </div>
       <div
-        className={`w-[65%] flex justify-end ${hidden ? "hidden" : ""}`}
-        onClick={() => setHidden(true)}
+        className={hidden ? "hidden" : ""}
+        onClick={(e) => e.stopPropagation()}
       >
-        <div onClick={(e) => e.stopPropagation()}>
-          <SketchPicker
-            color={color}
-            onChange={(color) => setColor(color.hex)}
-          />
-        </div>
+        <SketchPicker color={color} onChange={(color) => setColor(color.hex)} />
       </div>
     </div>
   );
